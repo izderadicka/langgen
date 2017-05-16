d2328368e6c99d10f1c485264b82b503f3fedaf2
Ivan <ivan.zderadicka@gmail.com>
2017-05-16 13:31:28 +0200

    Serve index file added README

---
+++ README.md
@@ -0,0 +1,16 @@
+LANGGEN or BILBO BABBITT BABBLES
+================================
+
+LANGGEN is my fun project to learn a [RUST](https://www.rust-lang.org).   It processes text files in given language (tested on English) and creates text model using [trigrams](https://en.wikipedia.org/wiki/Trigram). From these trigrams it generates random sentences, which are nonsence, but somehow remind "real" language.
+
+How to use
+==========
+
+1. Ensure you have latest RUST environment and cargo.
+2. Clone project from Github
+3. Get some text files to use as language corpus - for instance this file [all Doyle's Sherlock books in one txt](https://sherlock-holm.es/stories/plain-text/cano.txt) or repository contains script to download top 100 english books from [Project Guttenberg](https://www.gutenberg.org/).
+4. build and run http server `cargo run --bin serve --release -- [your_text_files]`
+
+License
+=======
+MIT or Apache 2.0
--- src/bin/serve.rs
+++ src/bin/serve.rs
@@ -4,13 +4,15 @@ extern crate rustc_serialize;
 extern crate docopt;
 
 use langgen::http_handler::generate_sentence;
-use std::path::Path;
 use docopt::Docopt;
 use hyper::server::{Server, Request, Response};
 use hyper::status::StatusCode;
 use hyper::method::Method;
+use self::hyper::mime::{self, Mime, TopLevel, SubLevel};
 use self::hyper::header;
-use std::io::Write;
+use std::io::{Write, stderr, copy};
+use std::path::{Path};
+use std::fs::File;
 
 const USAGE: &'static str = "
 Usage: serve [options] <file>...
@@ -18,6 +20,7 @@ Usage: serve [options] <file>...
 Options:
     -p, --port=<x>  listen on this port [default = 8088]
     -a, --addr=<h>  listen on this local address [defalt = 127.0.0.1] 
+    -i, --index=<i>  index file [default=./web/index.html]
     -h, --help  display this help and exit
     -v, --version  output version information and exit
 ";
@@ -27,6 +30,7 @@ struct Args {
     arg_file: Vec<String>,
     flag_port: Option<u16>,
     flag_addr: Option<String>,
+    flag_index: Option<String>
 }
 
 
@@ -38,6 +42,13 @@ fn main() {
     let port = args.flag_port.unwrap_or(8088);
     let addr = args.flag_addr.unwrap_or("127.0.0.1".to_string());
     let fnames = args.arg_file;
+    let default_index = "./web/index.html".to_string();
+    let index_file = args.flag_index.unwrap_or(default_index);
+    ;
+    if ! Path::new(&index_file).is_file() {
+        write!(stderr(), "Missing index file").unwrap();
+        std::process::exit(1);
+    }
     let mut trigrams = langgen::Trigrams::new();
     for fname in fnames {
         let i=langgen::FileTokenizer::new_from_path(Path::new(&fname)).expect("Cannot open file").into_iter();
@@ -56,10 +67,24 @@ fn main() {
     match req.method {
         Method::Get => {
             if let &self::hyper::uri::RequestUri::AbsolutePath(ref path) =&req.uri {
-            if path[..9]  == "/sentence"[..] {
+            if path.len()>=9 && path[..9]  == "/sentence"[..] {
                 generate_sentence(&req,res, &trigrams);
             } else {
-                *res.status_mut() = StatusCode::NotFound;
+                if let Ok(mut f) = File::open(&index_file) {
+                    let size = f.metadata().unwrap().len();
+                    {
+                    let headers= res.headers_mut();
+                    headers.set(header::ContentType(Mime(TopLevel::Text,
+                                             SubLevel::Html,
+                                             vec![(mime::Attr::Charset, mime::Value::Utf8)])));
+                    headers.set(header::ContentLength(size));
+                    }
+                    let mut res = res.start().unwrap();
+                    copy(&mut f,&mut res).unwrap();
+                    
+                } else {
+                *res.status_mut() = StatusCode::InternalServerError;
+                }
             }
             } else {
                 *res.status_mut() = StatusCode::BadRequest;
